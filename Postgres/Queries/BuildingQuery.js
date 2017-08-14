01880568c73fd218af850aa1a20a6df2fdbda1a4
Jimmy <j63guo@uwaterloo.ca>
2017-08-14 17:25:21 -0400

    get specific building info

--- Postgres/Queries/BuildingQuery.js
+++ Postgres/Queries/BuildingQuery.js
@@ -28,7 +28,7 @@ exports.get_buildings_info = (req, res, next) => {
                       FROM building a
                       INNER JOIN
                         (SELECT address_id, CONCAT(street_code, ' ', street_name, ', ', city, ', ', province, ', ', country) AS building_address, gps_x, gps_y
-+                        FROM address) b
+                        FROM address) b
                         ON a.address_id = b.address_id
                       INNER JOIN
                         (SELECT building_id, thumbnail, cover_photo FROM media WHERE building_id IS NOT NULL) c
@@ -36,6 +36,39 @@ exports.get_buildings_info = (req, res, next) => {
                       INNER JOIN
                         (SELECT building_id, MIN(price) AS min_price, MAX(price) AS max_price FROM room GROUP BY building_id) d
                         ON d.building_id = a.building_id
+                      `
+  const return_rows = (rows) => {
+    res.json(rows)
+  }
+  query(get_building)
+    .then((data) => {
+      return stringify_rows(data)
+    })
+    .then((data) => {
+      return log_through(data)
+    })
+    .then((data) => {
+      return return_rows(data)
+    })
+    .catch((error) => {
+        res.status(500).send('Failed to get buildings info')
+    })
+}
+
+exports.get_specific_building = (req, res, next) => {
+  const info = req.body
+  let get_building = `SELECT a.building_id, a.corporation_id, a.building_name,
+                             a.building_desc, a.building_type, b.building_address,
+                             b.gps_x, b.gps_y, c.thumbnail, c.cover_photo
+                      FROM building a
+                      INNER JOIN
+                        (SELECT address_id, CONCAT(street_code, ' ', street_name, ', ', city, ', ', province, ', ', country) AS building_address, gps_x, gps_y
+                        FROM address) b
+                        ON a.address_id = b.address_id
+                      INNER JOIN
+                        (SELECT building_id, thumbnail, cover_photo FROM media WHERE building_id IS NOT NULL) c
+                        ON a.building_id = c.building_id
+                      WHERE building_id = '${info.building_id}'
                       `
   const return_rows = (rows) => {
     res.json(rows)
