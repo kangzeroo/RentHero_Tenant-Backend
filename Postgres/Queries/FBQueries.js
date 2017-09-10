db9b6eb13ed83bc1cdfbd3bf2bafef183137c46f
Jimmy <j63guo@uwaterloo.ca>
2017-09-10 09:21:59 -0400

    asd

--- Postgres/Queries/FBQueries.js
+++ Postgres/Queries/FBQueries.js
@@ -22,13 +22,13 @@ exports.insert_facebook_sublets = (req, res, next) => {
   const info = req.body
   const values = [info.post_id, info.post_url, info.fb_user_id, info.fb_user_name, info.fb_user_pic,
                   info.price, info.address, info.description, info.gps_x, info.gps_y, info.ensuite_bath,
-                  info.utilities_included, info.female_only, info.rooms_available, info.location_id,
-                  info.fb_group_id, info.posted_date, info.phone]
+                  info.utils_included, info.female_only, info.rooms_left, info.location_id,
+                  info.fb_group_id, info.posted_date, info.phone, info.images]
 
   const insert_sublets = `INSERT INTO facebook_sublets (post_id, post_url, fb_user_id, fb_user_name, fb_user_pic,
-                                                        price, address, description, gps_x, gps_y, ensuite_bath, utilities_included,
-                                                        female_only, rooms_available, location_id, fb_group_id, posted_date, phone)
-                               SELECT $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18
+                                                        price, address, description, gps_x, gps_y, ensuite_bath, utils_included,
+                                                        female_only, rooms_left, location_id, fb_group_id, posted_date, phone, images)
+                               SELECT $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, $15, $16, $17, $18, $19
                                FROM facebook_sublets
                                WHERE NOT EXISTS (
                                  SELECT fb_user_id, fb_user_name, fb_user_pic, price, address
