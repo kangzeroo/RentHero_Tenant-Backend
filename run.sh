d2195aa3b7ef689e885e9836c7c88ef18767bc37
Kangze Huang <kangzeroo@gmail.com>
2017-09-22 06:28:09 -0400

    for prod

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run -d -it -p 3002:3002 --name=tenant_backend tenant_backend npm run prod -- --host=54.162.169.7
+docker run -d -it -p 3002:3002 --name=tenant_backend tenant_backend npm run prod -- --host=34.195.86.132
