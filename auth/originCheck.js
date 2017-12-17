504640a26d14e38f7936dcc0c72c37acc076e88a
Kangze Huang <kangzeroo@gmail.com>
2017-12-17 10:28:54 -0500

    originCheck modification

--- auth/originCheck.js
+++ auth/originCheck.js
@@ -13,6 +13,10 @@ exports.originCheck = function(req, res, next){
  } else {
    if (origin.indexOf('https://localhost:8081') > -1) {
      next()
+   } else if (origin.indexOf('http://localhost:3001') > -1 || origin.indexOf('https://localhost:3000') > -1) {
+     next()
+   } else if (origin.indexOf('ngrok.io') > -1) {
+     next()
    } else {
      res.status(500).send({
        message: 'Incorrect request origin. Not https://localhost:8081'
