2f927150d31e619e91c9581f3350e28904660d48
Kangze Huang <kangzeroo@gmail.com>
2018-01-06 14:39:25 -0500

    updated originCheck

--- auth/originCheck.js
+++ auth/originCheck.js
@@ -3,26 +3,17 @@
 exports.originCheck = function(req, res, next){
  const origin = req.get('origin')
  if (process.env.NODE_ENV === 'production') {
-   if (origin.indexOf('https://rentburrow.com') > -1) {
+   if (origin.indexOf('rentburrow.com') > -1 || origin.indexOf('renthero.ca') > -1) {
      next()
    } else {
-     // res.status(500).send({
-     //   message: 'Incorrect request origin. Not https://rentburrow.com'
-     // })
-     next()
+     res.status(500).send({
+       message: 'Bad boi bad boi'
+     })
    }
  } else {
-   if (origin.indexOf('https://localhost:8081') > -1) {
-     next()
-   } else if (origin.indexOf('http://localhost:3001') > -1 || origin.indexOf('https://localhost:3000') > -1) {
-     next()
-   } else if (origin.indexOf('ngrok.io') > -1) {
-     next()
-   } else {
-     next()
-     // res.status(500).send({
-     //   message: 'Incorrect request origin. Not https://localhost:8081'
-     // })
-   }
+   next()
+   // res.status(500).send({
+   //   message: 'Incorrect request origin. Not https://localhost:8081'
+   // })
  }
 }
